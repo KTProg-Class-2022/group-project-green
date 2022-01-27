1b298ddb99e53a0a2728c5f3ea4aab19ca2d4995
nzuliy <nzuliy2022@jpkeefehs.org>
2022-01-27 12:31:41 -0500

    NVM last change was in the wrong file

--- Simple Base Code/Item.cs
+++ Simple Base Code/Item.cs
@@ -4,34 +4,6 @@ using System.Text;
 
 namespace Simple_Base_Code
 {
-	class Commands
-    {
-        //nick ge the ability for us to input commands from console working. That Is all Very Cool
-        public Commands()
-        {
-
-        }
-
-        public void addItem()
-        {
-           
-        }
-        public void BosnianRoulette()
-        {
-            //Vaansh will work on this
-        }
-
-        public void selectRoom()
-        {
-            //future command
-        }
-
-        public void StartMastermind()
-        {
-            //bruno
-        }
-
-    }
     class Item
     {
 		private string _name;
